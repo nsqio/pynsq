e3fb273ca16feed66233ccaf8fb1d8b010deca92
Jehiah Czebotar <jehiah@gmail.com>
2014-02-15 23:28:28 -0500

    connection attributes not client

--- nsq/client.py
+++ nsq/client.py
@@ -13,10 +13,10 @@ class Client(object):
 
     def _on_connection_identify_response(self, conn, data, **kwargs):
         logging.info('[%s:%s] IDENTIFY received %r' % (conn.id, self.name, data))
-        if self.tls_v1 and not data.get('tls_v1'):
+        if conn.tls_v1 and not data.get('tls_v1'):
             logging.warning('[%s:%s] tls_v1 requested but disabled, could not negotiate feature',
                             conn.id, self.name)
-        if self.snappy and not data.get('snappy'):
+        if conn.snappy and not data.get('snappy'):
             logging.warning('[%s:%s] snappy requested but disabled, could not negotiate feature',
                             conn.id, self.name)
 
