c2cd61323fdd1f10653ee347a50d86036678c58b
Stan Hu <stanhu@aclimalabs.com>
2014-07-12 08:39:49 -0700

    Check keyword arguments passed to Reader constructor. Throw an AssertionError if an invalid argument is passed.
    Before this change, specifying an invalid keyword argument would cause the AsyncConn
    to fail quietly in the Tornado default exception handler during run-time.
    
    Remove trailing whitespace
    
    Check keyword arguments in Writer constructor and add unit test

---
+++ tests/test_writer.py
@@ -0,0 +1,28 @@
+import nsq
+import unittest
+
+
+class WriterUnitTest(unittest.TestCase):
+
+    def setUp(self):
+        super(WriterUnitTest, self).setUp()
+
+    def test_constructor(self):
+        name = 'test'
+        reconnect_interval = 10.0
+        writer = nsq.Writer(nsqd_tcp_addresses=['127.0.0.1:4150'],
+                            reconnect_interval=reconnect_interval,
+                            name=name)
+        self.assertEqual(writer.name, name)
+        self.assertEqual(0, len(writer.conn_kwargs))
+        self.assertEqual(writer.reconnect_interval, reconnect_interval)
+
+    def test_bad_writer_arguments(self):
+        bad_options = dict(foo=10)
+
+        self.assertRaises(
+            AssertionError,
+            nsq.Writer,
+            nsqd_tcp_addresses=['127.0.0.1:4150'],
+            reconnect_interval=15.0,
+            name='test', **bad_options)
