11297a0dc348f82c6a8c7cd5f6224f924a2cdb49
Matt Reiferson <mreiferson@gmail.com>
2018-03-05 14:11:23 -0800

    bump: v0.8.3-alpha

--- nsq/version.py
+++ nsq/version.py
@@ -1,2 +1,2 @@
 # also update in setup.py
-__version__ = '0.8.2'
+__version__ = '0.8.3-alpha'
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class PyTest(TestCommand):
 
 
 # also update in nsq/version.py
-version = '0.8.2'
+version = '0.8.3-alpha'
 
 
 setup(
