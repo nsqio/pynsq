b6d2bc7818319af07d6c7c0c6e9bf9a2880322e3
Matt Reiferson <mreiferson@gmail.com>
2020-06-18 18:11:53 -0700

    bump to 0.9.0-beta2

--- nsq/version.py
+++ nsq/version.py
@@ -1,2 +1,2 @@
 # also update in setup.py
-__version__ = '0.9.0-beta1'
+__version__ = '0.9.0-beta2'
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class PyTest(TestCommand):
 
 
 # also update in nsq/version.py
-version = '0.9.0b1'
+version = '0.9.0-beta2'
 
 
 setup(
